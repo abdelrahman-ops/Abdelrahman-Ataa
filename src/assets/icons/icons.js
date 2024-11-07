d268f873239143d2be903a2ca89f87ad245639d2
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-11-08 01:55:41 +0200

    assets used

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,4 @@
+import { FaGithub , FaWhatsapp } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdOutlineMailOutline , MdOutlineLocationOn } from "react-icons/md";
