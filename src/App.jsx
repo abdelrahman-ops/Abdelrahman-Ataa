1da7660eaac50e5f0bfbe792d08d94916938c0b2
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-11-08 01:55:04 +0200

    App.jsx demonstrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-// src/App.jsx
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+// import Hero from './components/Hero';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+
+
+
 
 function App() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow container mx-auto p-4">
-        <h1 className="text-3xl font-bold text-center my-8">Welcome to My Portfolio</h1>
-        <p className="text-center">This is where you can showcase your work and skills.</p>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<>
+			<div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] bg-[#F5F5F5] font-maven">
+				<Navbar />
+				<Profile />
+				<About />
+				<Skills />
+				<Projects />
+				<Services />
+				<Contact />
+				<Footer />
+			</div>
+			
+		</>
+		
+	);
 }
 
 export default App;
