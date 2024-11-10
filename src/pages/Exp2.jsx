c81fb5e8e9825494029bfaa76d51d1363bd5424c
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-11-10 04:27:16 +0200

    add some motions

--- src/pages/Exp2.jsx
+++ src/pages/Exp2.jsx
@@ -12,8 +12,8 @@ const Exp2 = () => {
     };
     
     const { ref, inView } = useInView({
-        triggerOnce: true, // Trigger animation only once when it enters view
-        threshold: 0.3, // Trigger when 30% of the section is in view
+        triggerOnce: true,
+        threshold: 0.3,
     });
     return (
         <motion.div
@@ -21,8 +21,9 @@ const Exp2 = () => {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
             transition={{ duration: 0.6 }}
+            className="my-20"
         >
-            <section className="w-full border  shadow-md rounded-lg p-6">
+            <div className="w-full border  shadow-md rounded-lg p-6">
                 <div className="flex justify-center items-center flex-wrap mb-5">
                     <div>
                         <p className="text-4xl font-semibold text-center">
@@ -37,14 +38,14 @@ const Exp2 = () => {
                         
                         {/* Button 1 Education */}
                         <div 
-                            className = {`qualification__button inline-flex items-center mx-4 my-0 ${toggleState === 1 ? 'qualification__active' : ''} `}
+                            className = {`inline-flex items-center mx-4 my-0 ${toggleState === 1 ? 'text-slate-900' : ''} `}
                             onClick={() => toggleTab(1)}
                         >
                             <i className="uil uil-graduation-cap qualification__icon"></i>Education
                         </div>
                         
                         {/* Button 2 Experience */}
-                        <div className = {`qualification__button inline-flex items-center mx-4 my-0 ${toggleState === 2 ? 'qualification__active' : ''} `}
+                        <div className = {`inline-flex items-center mx-4 my-0 ${toggleState === 2 ? 'text-slate-900' : ''} `}
                             onClick={() => toggleTab(2)}
                         >
                             <i className="uil uil-briefcase-alt qualification__icon "></i>Experience
@@ -55,17 +56,20 @@ const Exp2 = () => {
                     <div className="grid grid-cols-[0.5fr] justify-center">
                         {/* Left Column: Education */}
                         <div className={`${toggleState === 1 ? 'block' : 'hidden'}`}>
-                            {/* Qualification Data Entries */}
                             
                             {/* Data 1 */}
                             <div className="qualification__data">
-                                <div>
+                                <motion.div
+                                    initial={{ opacity: 0, x: -100 }}
+                                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }}
+                                    transition={{ duration: 0.6, delay: 0.2 }}
+                                >
                                     <h3 className="qualification__title">Web Design</h3>
                                     <span className="qualification__subtitle">Spain - Ins</span>
                                     <div className="qualification__calendar">
                                         <i className="uil uil-calendar-alt">2021 - Present</i>
                                     </div>
-                                </div>
+                                </motion.div>
                                 <div>
                                     <span className="qualification__rounder"></span>
                                     <span className="qualification__line"></span>
@@ -81,25 +85,33 @@ const Exp2 = () => {
                                     <span className="qualification__line"></span>
                                 </div>
                                 
-                                <div>
+                                <motion.div
+                                    initial={{ opacity: 0, x: 100 }}
+                                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 100 }}
+                                    transition={{ duration: 0.6, delay: 0.4 }}
+                                >
                                     <h3 className="qualification__title">Web Design</h3>
                                     <span className="qualification__subtitle">Spain - Ins</span>
                                     <div className="qualification__calendar">
                                         <i className="uil uil-calendar-alt"></i>2021 - Present
                                     </div>
-                                </div>
+                                </motion.div>
                                 
                             </div>
                             
                             {/* Data 3 */}
                             <div className="qualification__data">
-                                <div>
+                                <motion.div
+                                    initial={{ opacity: 0, x: -100 }}
+                                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 100 }}
+                                    transition={{ duration: 0.6, delay: 0.6 }}
+                                >
                                     <h3 className="qualification__title">Web dev</h3>
                                     <span className="qualification__subtitle">Spain - Ins</span>
                                     <div className="qualification__calendar">
                                         <i className="uil uil-calendar-alt">2021 - Present</i>
                                     </div>
-                                </div>
+                                </motion.div>
                                 <div>
                                     <span className="qualification__rounder"></span>
                                     <span className="qualification__line"></span>
@@ -115,13 +127,17 @@ const Exp2 = () => {
                                     <span className="qualification__line"></span>
                                 </div>
                                 
-                                <div>
+                                <motion.div
+                                    initial={{ opacity: 0, x: 100 }}
+                                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 100 }}
+                                    transition={{ duration: 0.6, delay: 0.8 }}
+                                >
                                     <h3 className="qualification__title">UX Thing</h3>
                                     <span className="qualification__subtitle">Spain - Ins</span>
                                     <div className="qualification__calendar">
                                         <i className="uil uil-calendar-alt">2021 - Present</i>
                                     </div>
-                                </div>
+                                </motion.div>
                                 
                             </div>
                         </div>
@@ -181,7 +197,7 @@ const Exp2 = () => {
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </motion.div>
     );
 };
